dc6b777e8ac3c97cd4735681e301605e8c6a00a0
Jesus Villca Apaza <24villca@gmail.com>
2020-01-19 18:32:10 -0400

    Update tcpdf.php

--- config/tcpdf.php
+++ config/tcpdf.php
@@ -1,16 +1,17 @@
 <?php
 return [
-	'page_format'           => 'A4',
-	'page_orientation'      => 'P',
-	'page_units'            => 'mm',
-	'unicode'               => true,
-	'encoding'              => 'UTF-8',
-	'font_directory'        => '',
-	'image_directory'       => '',
-	'tcpdf_throw_exception' => false,
-	'use_fpdi'              => false,
-	'use_original_header'   => false,
-	'use_original_footer'   => false,
+    'page_format'           => 'A4',
+    'page_orientation'      => 'P',
+    'page_units'            => 'mm',
+    'unicode'               => true,
+    'encoding'              => 'UTF-8',
+    'font_directory'        => '',
+    'image_directory'       => '',
+    'tcpdf_throw_exception' => false,
+    'use_fpdi'              => false,
+    'use_original_header'   => false,
+    'use_original_footer'   => false,
+    'pdfa'                  => false, // Options: false, 1, 3
 
     /***
      * See more info
